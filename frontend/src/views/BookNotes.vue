ce87e11b8acef076e7ee02c33c075136d35ebc45
Nikita <nerdyaus21@gmail.com>
2024-07-01 19:19:44 +0300

    feat [frontend]: added tabs learning/notes

--- frontend/src/views/BookNotes.vue
+++ frontend/src/views/BookNotes.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
         <h1 class="mt-2 text-uppercase ">Book notes</h1>
-        <div> </div>
+        <div width="500px">
+            <v-tabs bg-color="grey-darken-2" fixed-tabs>
+                <v-tab><v-icon icon="mdi-brain"></v-icon>
+                    <h5 class="ml-2">Learning</h5>
+                </v-tab>
+                <v-tab><v-icon icon="mdi-pencil"></v-icon>
+                    <h5 class="ml-2">Notes</h5>
+                </v-tab>
+            </v-tabs>
+        </div>
     </div>
 </template>
 
