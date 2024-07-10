ff35e1ec8033c535b7b3c9da80f7488942e31301
Nikita <nerdyaus21@gmail.com>
2024-07-10 21:41:03 +0300

    feat [front+back]: created tables for videos, added pages videos,settings

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import BooksList from '../views/BooksList.vue'
 import BookNotes from '../views/BookNotes.vue'
+import VideosList from '../views/VideosList.vue'
+import Settings from '../views/SettingsPage.vue'
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/books',
     name: 'BooksList',
     component: BooksList
+  },
+  {
+    path: '/videos',
+    name: 'VideosList',
+    component: VideosList
+  },
+  {
+    path: '/settings',
+    name: 'SettingsPage',
+    component: Settings
   },
   {
     path: '/book/:id/notes',
---
+++ frontend/src/views/SettingsPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1 class="mt-2 text-uppercase">Settings</h1>
+
+    </div>
+</template>
+
+<script>
+
+
+
+</script>
+
+<style>
+.new-line {
+    white-space: pre-wrap;
+}
+</style>
